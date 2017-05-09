ef5e661f7d57631b4ddf8d71be1ec57b95d25b21
李航 <1244225737@qq.com>
2017-05-09 15:57:04 +0800

    时间格式更改

--- src/main/java/com/neuq/info/common/format/CustomDateSerializer.java
+++ src/main/java/com/neuq/info/common/format/CustomDateSerializer.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -17,7 +18,39 @@ public class CustomDateSerializer extends JsonSerializer<Date> {
         @Override
         public void serialize(Date value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException,
                 JsonProcessingException {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            jsonGenerator.writeString(sdf.format(value));
+            Calendar now = Calendar.getInstance();
+            int nowYear= now.get(Calendar.YEAR);
+            int nowMonth= now.get(Calendar.MONTH) + 1;
+            int nowDay= now.get(Calendar.DAY_OF_MONTH);
+            int nowHour= now.get(Calendar.HOUR);
+            int nowMinute= now.get(Calendar.MINUTE);
+            int nowSecond= now.get(Calendar.SECOND);
+            Calendar post=Calendar.getInstance();
+            post.setTime(value);
+            int postYear= post.get(Calendar.YEAR);
+            int postMonth= post.get(Calendar.MONTH) + 1;
+            int postDay= post.get(Calendar.DAY_OF_MONTH);
+            int postHour= post.get(Calendar.HOUR);
+            int postMinute= post.get(Calendar.MINUTE);
+            int postSecond= post.get(Calendar.SECOND);
+            String result;
+
+            if(nowYear - postYear >= 1){
+                result = (nowYear - postYear) + "年前";
+            }else if(nowMonth - postMonth >= 1 || nowDay - postDay > 2){
+                result = postMonth + "月" + postDay + "日" + " " + postHour + ":" + postMinute;
+            }else if(nowDay - postDay >= 1 && nowDay - postDay <=2 ){
+                result = nowDay - postDay == 1 ? ("昨天" + postHour + ":" + postMinute) : ("前天" + postHour + ":" + postMinute);
+            }else if(nowHour - postHour >= 1){
+                result = "今天" + postHour + ":" + postMinute;
+            }else if(nowMinute - postMinute >=1) {
+                result = (nowMinute - postMinute) + "分钟前";
+            } else if(nowSecond - postSecond >=1){
+                    result = (nowSecond - postSecond) + "秒前";
+            }else{
+                result = "刚刚";
+            }
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            jsonGenerator.writeString(result);
         }
     }
